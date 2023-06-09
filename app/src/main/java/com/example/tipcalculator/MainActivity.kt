a800e5229a54d7c972822f09462439e0bbfc54eb
utsab6991@gmail.com <Dopeninj@1996>
2023-06-09 17:09:13 +1000

    adding instrumentation test

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.view.get
 import com.example.tipcalculator.databinding.ActivityMainBinding
@@ -16,9 +20,12 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.calculateButton.setOnClickListener {
             calculateTip()
+        }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+
         }
     }
 
@@ -53,4 +60,15 @@ class MainActivity : AppCompatActivity() {
         binding.tipResult.text=getString(R.string.tip_amount,formattedTip)
     }
 
+    private fun handleKeyEvent (view: View, keyCode:Int): Boolean{
+        Toast.makeText(this,"Test",Toast.LENGTH_SHORT).show()
+        if(keyCode == KeyEvent.KEYCODE_ENTER){
+
+            //Hide the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        else return true
+    }
 }
